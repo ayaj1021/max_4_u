cb506edc8fe6794a0776af97c00dde17d4325b91
ayaj1021 <120981954+ayaj1021@users.noreply.github.com>
2024-12-18 20:58:43 +0100

    fixed issue on max4u

--- lib/app/presentation/features/vendor_features/presentation/customers_section/customers_transaction_screen.dart
+++ lib/app/presentation/features/vendor_features/presentation/customers_section/customers_transaction_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:max_4_u/app/enums/month_dropdown_enum.dart';
+import 'package:intl/intl.dart';
+import 'package:max_4_u/app/presentation/features/admin/provider/get_user_transactions_by_id_provider.dart';
 import 'package:max_4_u/app/presentation/features/dashboard/home/component/transaction_history_component.dart';
+import 'package:max_4_u/app/presentation/features/dashboard/transaction/presentation/views/widgets/empty_transaction_section.dart';
 import 'package:max_4_u/app/styles/app_colors.dart';
 import 'package:max_4_u/app/styles/app_text_styles.dart';
 import 'package:max_4_u/app/utils/white_space.dart';
+import 'package:provider/provider.dart';
 
 class CustomerTransactionScreen extends StatefulWidget {
   const CustomerTransactionScreen({super.key});
@@ -15,7 +17,16 @@ class CustomerTransactionScreen extends StatefulWidget {
 }
 
 class _CustomerTransactionScreenState extends State<CustomerTransactionScreen> {
-  Months _selectedMonth = Months.January;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<GetUserTransactionProvider>(context, listen: false)
+          .userTransactionsById(userId: '1');
+    });
+
+    super.initState();
+  }
+
   final List icons = [
     'assets/icons/call_icon.png',
     'assets/icons/data_icon.png',
@@ -25,150 +36,66 @@ class _CustomerTransactionScreenState extends State<CustomerTransactionScreen> {
     Color(0xffDEEDF7),
     Color(0xffD6DDFE),
     Color(0xffE8D6FE),
-
   ];
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            DropdownButton<Months>(
-              underline: const SizedBox(),
-              value: _selectedMonth,
-              items: Months.values.map((Months month) {
-                return DropdownMenuItem(
-                    value: month,
-                    child: Container(
-                        padding: EdgeInsets.zero,
-                        child: Text(_monthToString(month))));
-              }).toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  _selectedMonth = newValue!;
-                });
-              },
-            ),
-            GestureDetector(
-              onTap: () {
-                showModalBottomSheet(
-                    context: context,
-                    builder: (context) {
-                      return Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 21),
-                        height: 508.h,
-                        width: MediaQuery.of(context).size.width,
-                        decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(24),
-                              topRight: Radius.circular(24),
-                            ),
-                            color: AppColors.whiteColor),
-                        child: Column(
-                          children: [
-                            Align(
-                              alignment: Alignment.topRight,
-                              child: SizedBox(
-                                height: 24,
-                                width: 24,
-                                child:
-                                    Image.asset('assets/icons/cancel_icon.png'),
-                              ),
-                            ),
-                            verticalSpace(2),
-                          ],
-                        ),
-                      );
-                    });
-              },
-              child: Row(
-                children: [
-                  Text(
-                    'Filter',
-                    style: AppTextStyles.font16,
-                  ),
-                  Icon(Icons.filter_alt_outlined)
-                ],
-              ),
-            ),
-          ],
-        ),
-        verticalSpace(25),
-        Expanded(
-          child: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (_, index) {
-                    return GestureDetector(
-                      // onTap: () => nextScreen(
-                      //     context, const TransactionDetailsScreen()),
-                      child: Column(
-                        children: [
-                          TransactionSection(
-                            transactionIcon: icons[2],
-                            transactionType: 'Added funds',
-                            transactionDate: 'Apr 18th, 20:59',
-                            transactionAmount: '-N35,000.00',
-                            transactionStatus: 'Successful',
-                            transactionColor:
-
-                            // data.subType!.contains('card')
-                            //               ? colors[1]
-                            //               : data.subType!.contains('data')
-                            //                   ? colors[2]
-                            //                   : colors[0],
-                            
-                             Color(0xffD6DDFE),
-                            transactionStatusColor: Colors.green,
-                            transactionNumber: '',
-                          ),
-                          verticalSpace(8),
-                          Divider(
-                            color: AppColors.blackColor.withOpacity(0.1),
-                          ),
-                          verticalSpace(8),
-                        ],
+    DateFormat dateFormat = DateFormat('MMMM d, yyyy h:mm a');
+    return Consumer<GetUserTransactionProvider>(
+      builder: (context, userTransaction, _) {
+        final dataList = userTransaction.userTransactions.data?.responseData!;
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              dataList == null
+                  ? Center(
+                      child: Text(
+                        'No data',
+                        style: AppTextStyles.font14.copyWith(
+                            color: AppColors.textColor,
+                            fontWeight: FontWeight.w400),
                       ),
-                    );
-                  })),
-        ),
-      ],
+                    )
+                  : dataList.isEmpty
+                      ? EmptyTransactionSection()
+                      : SizedBox(
+                          height: MediaQuery.of(context).size.height,
+                          child: ListView.builder(
+                              physics: AlwaysScrollableScrollPhysics(),
+                              itemCount: dataList.length,
+                              // userTransaction.userTransactions.data?.responseData?.length,
+                              itemBuilder: (_, index) {
+                                final data = userTransaction.userTransactions
+                                    .data?.responseData?[index];
+
+                                return Column(children: [
+                                  TransactionSection(
+                                    transactionIcon: icons[2],
+                                    transactionType: '${data?.purchaseType}',
+                                    transactionDate:
+                                        '${dateFormat.format(data?.regDate as DateTime)}',
+                                    transactionAmount: 'N${data?.amountPaid}',
+                                    transactionStatus: '${data?.status}',
+                                    transactionColor: Color(0xffD6DDFE),
+                                    transactionStatusColor:
+                                        data?.status == 'success'
+                                            ? Colors.green
+                                            : data?.status == 'pending'
+                                                ? Color(0xffA6B309)
+                                                : Colors.red,
+                                    transactionNumber: '${data?.number ?? ''}',
+                                  ),
+                                  verticalSpace(8),
+                                  Divider(
+                                    color:
+                                        AppColors.blackColor.withOpacity(0.1),
+                                  )
+                                ]);
+                              }),
+                        ),
+            ],
+          ),
+        );
+      },
     );
-  }
-}
-
-String _monthToString(Months month) {
-  switch (month) {
-    case Months.January:
-      return 'January';
-    case Months.February:
-      return 'February';
-    case Months.March:
-      return 'March';
-    case Months.April:
-      return 'April';
-    case Months.May:
-      return 'May';
-    case Months.June:
-      return 'June';
-    case Months.July:
-      return 'July';
-    case Months.August:
-      return 'August';
-    case Months.September:
-      return 'September';
-    case Months.October:
-      return 'October';
-    case Months.November:
-      return 'November';
-    case Months.December:
-      return 'December';
-
-    default:
-      return '';
   }
 }
